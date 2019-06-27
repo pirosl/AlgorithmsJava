2147c5984cc7bf66da8e7bd219c068c21c697c1a
Lucian Piros <pirosl@yahoo.com>
2019-06-27 09:53:19 +0100

    Add balancing to quick union alg

--- src/main/java/com/lucianpiros/algorithms/unionfind/QuickUnionUF.java
+++ src/main/java/com/lucianpiros/algorithms/unionfind/QuickUnionUF.java
@@ -2,11 +2,14 @@ package com.lucianpiros.algorithms.unionfind;
 
 public class QuickUnionUF implements UnionFind {
 	private int[] id;
+	private int[] size;
 
 	public QuickUnionUF(int N) {
 		id = new int[N];
+		size = new int[N];
 		for(int i = 0; i < N; i++) {
 			id[i] = i;
+			size[i] = 1;
 		}
 	}
 
@@ -25,6 +28,16 @@ public class QuickUnionUF implements UnionFind {
 	public void union(int p, int q) {
 		int rp = root(p);
 		int rq = root(q);
-		id[rp] = rq;
+
+		if(rp != rq) {
+			if(size[rp] < size[rq]) {
+				id[rp] = rq;
+				size[rq] += size[rp];
+			}
+			else {
+				id[rq] = rp;
+				size[rp] += size[rq];	
+			}
+		}
 	}
 }
